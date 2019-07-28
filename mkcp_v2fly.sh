30228a6d3762fd0c515449522a0514400fc3da6e
LGTMXD <1430786998@QQ.COM>
2019-07-28 12:12:51 +0800

    Add files via upload

---
+++ mkcp_v2fly.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#ip:9184
+
+apt-get update
+echo "1
+6
+
+
+Y
+
+
+
+
+"|bash <(curl -sL git.io/v2ray.sh)
+
+echo "1
+"|bash <(curl -s -L https://233blog.com/filebrowser.sh)
+
+
+wget https://github.com/Cenmrev/V2RayX/releases/download/v1.5.1/V2RayX.app.zip
+wget https://github.com/Cenmrev/V2RayW/releases/download/v1.0.0-beta2/V2RayW.zip
+wget https://github.com/2dust/v2rayNG/releases/download/1.0.1/app-universal-release.apk
+echo "ios https://app.aneeo.com/show?id=88">ios.txt
+v2ray qr > 123.txt
+v2ray link >> 123.txt
+v2ray ssqr >> 123.txt
+
+mv 123.txt ios.txt V2RayX.app.zip V2RayW.zip app-universal-release.apk /etc/filebrowser/
+
+// Photo by Jordi Moncasi on Unsplash
